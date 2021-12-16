02346a624be2440da40662e58b92e35cc61563fa
“Victoria <victoriahall.dev@gmail.com>
2021-12-16 01:32:55 +0000

    Pair programming Word Search assignment

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,20 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
+    let string = false;
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    for (let l of horizontalJoin) {
+        let verify = 0;
+        for (let letter of word) {
+            let index = l.indexOf(letter)
+            if (index !== -1) {
+                l = l.slice(index);
+                verify++;
+            }
+        }
+        if (verify === word.length) {
+            string = true;
+        }
     }
-}
+    return string;
+};
 
 module.exports = wordSearch
